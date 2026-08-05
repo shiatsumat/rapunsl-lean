13cce3a95168166b68789669bc18397862697785
Yusuke Matsushita <ysk.m24t@gmail.com>
2026-08-05 19:45:09 +0200

    Use attribute command in Logic.BI

--- RapunSL/Logic/BI.lean
+++ RapunSL/Logic/BI.lean
@@ -16,30 +16,22 @@ variable {PROP} [BI PROP] (P Q R S : PROP)
 
 /-! ## `⊢` is a preorder -/
 
-@[refl] lemma entails_refl' : P ⊢ P := by
-  apply entails_refl
-
-@[trans] lemma entails_trans'' : (P ⊢ Q) → (Q ⊢ R) → P ⊢ R := by
-  apply Trans.trans
+attribute [refl] entails_refl
+attribute [trans] entails_trans
 
 instance entails_instPreorder : IsPreorder PROP Entails where
-  refl := entails_refl'
+  refl := by intro _; rfl
 
 /-! ## `⊣⊢` is an equivalence relation -/
 
-@[refl] lemma bi_entails_refl : P ⊣⊢ P := by
-  constructor <;> rfl
-
-@[symm] lemma bi_entails_symm : P ⊣⊢ Q → Q ⊣⊢ P := by
-  intro ⟨_, _⟩; constructor <;> trivial
-
-@[trans] lemma bi_entails_trans : P ⊣⊢ Q → Q ⊣⊢ R → P ⊣⊢ R := by
-  apply Trans.trans
+attribute [refl] BIBase.BiEntails.rfl
+attribute [symm] BIBase.BiEntails.symm
+attribute [trans] BIBase.BiEntails.trans
 
 instance bi_entails_instIsEquiv : IsEquiv PROP BiEntails where
-  refl := bi_entails_refl
-  symm := bi_entails_symm
-  trans := bi_entails_trans
+  refl := by intro _; rfl
+  symm := by intro _ _ _; symm; trivial
+  trans := by intro _ _ _ _ _; trans <;> assumption
 
 /-! ## Reinterpretation of connectives -/
 
@@ -57,34 +49,8 @@ lemma false_as_exists :
 
 /-! ## `gcongr` lemmas -/
 
-@[gcongr] lemma and_mono' : (P ⊢ Q) → (R ⊢ S) → P ∧ R ⊢ Q ∧ S := by
-  apply and_mono
-
-@[gcongr] lemma or_mono' : (P ⊢ Q) → (R ⊢ S) → P ∨ R ⊢ Q ∨ S := by
-  apply or_mono
-
-@[gcongr] lemma imp_mono' : (Q ⊢ P) → (R ⊢ S) → (P → R) ⊢ (Q → S) := by
-  apply imp_mono
-
-@[gcongr] lemma forall_mono' {α : Sort*} (P Q : α → PROP) :
-    (∀ a, P a ⊢ Q a) → (∀ a, P a) ⊢ (∀ a, Q a) := by
-  apply forall_mono
-
-@[gcongr] lemma exists_mono' {α : Sort*} (P Q : α → PROP) :
-    (∀ a, P a ⊢ Q a) → (∃ a, P a) ⊢ (∃ a, Q a) := by
-  apply exists_mono
-
-@[gcongr] lemma sep_mono' : (P ⊢ Q) → (R ⊢ S) → P ∗ R ⊢ Q ∗ S := by
-  apply sep_mono
-
-@[gcongr] lemma wand_mono' : (Q ⊢ P) → (R ⊢ S) → (P -∗ R) ⊢ Q -∗ S := by
-  apply wand_mono
-
-@[gcongr] lemma persistently_mono' : (P ⊢ Q) → <pers> P ⊢ <pers> Q := by
-  apply persistently_mono
-
-@[gcongr] lemma later_mono' : (P ⊢ Q) → ▷ P ⊢ ▷ Q := by
-  apply later_mono
+attribute [gcongr] and_mono or_mono imp_mono forall_mono exists_mono sep_mono wand_mono
+  persistently_mono later_mono
 
 end Iris.BI
 
@@ -93,14 +59,13 @@ end Iris.BI
 class Iris.BIE PROP extends Iris.BI PROP where
   bi_ext : ∀ P Q : PROP, (P ⊣⊢ Q) → P = Q
 
+attribute [ext] Iris.BIE.bi_ext
+
 macro:25 P:term:29 " =ᴮᴵ " Q:term:29 : term => `(Eq iprop($P) iprop($Q))
 
 namespace Iris.BI
 
 variable {PROP} [BIE PROP] (P Q : PROP)
-
-@[ext]
-lemma bi_ext : (P ⊣⊢ Q) → P = Q := by apply Iris.BIE.bi_ext
 
 lemma or_as_exists' : P ∨ Q =ᴮᴵ ∃ b : Bool, if b then P else Q := by
   ext1; apply or_as_exists
