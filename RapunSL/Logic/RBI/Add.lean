a69b6e95a0a656fde9975476cb2d0f31b142dffd
Yusuke Matsushita <ysk.m24t@gmail.com>
2026-08-06 15:53:17 +0200

    Refactor RBI.Add to separate rmadd-related things

--- RapunSL/Logic/RBI/Add.lean
+++ RapunSL/Logic/RBI/Add.lean
@@ -11,7 +11,7 @@ namespace RBI
 variable {ρ : Type u} [RR ρ] (P P' Q Q' R : RProp ρ)
   (A B C AB ABC : Mset ρ)
 
-/-! ## Sum connectives -/
+/-! ## Addition of multisets -/
 
 /-- Addition of `Mset`s -/
 def rmadd (A B C : Mset ρ) : Prop :=
@@ -21,37 +21,12 @@ def rmadd (A B C : Mset ρ) : Prop :=
 
 scoped macro:50 A:term:50 " +ᴿᴹ " B:term " =ᴿᴹ " C:term:50 : term => `(RBI.rmadd $A $B $C)
 
-/-- Binary sum over `RProp` -/
-instance RProp.instAdd : Add (RProp ρ) where
-  add P Q := .mk fun C ↦ ∃ A B, A ∈ P ∧ B ∈ Q ∧
-    A.val.val +ᴿᴹ B.val.val =ᴿᴹ C.val.val
-
-/-- Unfold `+` for `RProp` -/
-lemma add_unfold :
-    (HAdd.hAdd : RProp ρ → RProp ρ → RProp ρ) =
-      fun P Q => .mk fun C ↦ ∃ A B, A ∈ P ∧ B ∈ Q ∧
-        A.val.val +ᴿᴹ B.val.val =ᴿᴹ C.val.val := rfl
-
-/-- `+` is monotone -/
-@[gcongr] lemma add_mono : (P ⊢ P') → (Q ⊢ Q') → P + Q ⊢ P' + Q' := by
-  intro PP' QQ' _ ⟨A, B, _, _, _⟩; exists A, B; and_intros;
-  { apply PP'; trivial }; { apply QQ'; trivial }; trivial
-
 /-- `+ᴿᴹ` is commutative -/
 lemma rmadd_comm' : A +ᴿᴹ B =ᴿᴹ C → B +ᴿᴹ A =ᴿᴹ C := by
   rintro ⟨AB, coh, rfl⟩; exists AB.symm; and_intros;
   · intro _ _; rw [Bij.symm_graph_mem]; intro _; symm; apply coh; trivial
   · simp only [Bij.symm_graph, ←Mset.comp_map];
     congr; ext1 ⟨a, b⟩; rw [add_comm a b]; rfl
-
-/-- `+` is commutative -/
-private lemma add_comm' : P + Q ⊢ Q + P := by
-  intro _ ⟨A, B, _, _, _⟩; exists B, A; and_intros; { trivial }; { trivial };
-  apply rmadd_comm'; trivial
-
-/-- `+` is commutative -/
-instance RProp.instAddCommMagma : AddCommMagma (RProp ρ) where
-  add_comm := by intro _ _; ext; constructor <;> apply add_comm'
 
 /-- Construct `+ᴿᴹ` from a multiset of coherent pairs -/
 lemma pairs_rmadd (S : Mset (ρ × ρ)) :
@@ -119,6 +94,33 @@ lemma rmadd_assoc_l :
     · rfl
     · rw [←hgr, ←Mset.comp_map]; apply Mset.map_congr;
       rintro ⟨⟨a, b⟩, c⟩ mem; exact (pw _ _ _ mem).2.2.symm
+
+/-! ## Sum connectives -/
+
+/-- Binary sum over `RProp` -/
+instance RProp.instAdd : Add (RProp ρ) where
+  add P Q := .mk fun C ↦ ∃ A B, A ∈ P ∧ B ∈ Q ∧
+    A.val.val +ᴿᴹ B.val.val =ᴿᴹ C.val.val
+
+/-- Unfold `+` for `RProp` -/
+lemma add_unfold :
+    (HAdd.hAdd : RProp ρ → RProp ρ → RProp ρ) =
+      fun P Q => .mk fun C ↦ ∃ A B, A ∈ P ∧ B ∈ Q ∧
+        A.val.val +ᴿᴹ B.val.val =ᴿᴹ C.val.val := rfl
+
+/-- `+` is monotone -/
+@[gcongr] lemma add_mono : (P ⊢ P') → (Q ⊢ Q') → P + Q ⊢ P' + Q' := by
+  intro PP' QQ' _ ⟨A, B, _, _, _⟩; exists A, B; and_intros;
+  { apply PP'; trivial }; { apply QQ'; trivial }; trivial
+
+/-- `+` is commutative -/
+private lemma add_comm' : P + Q ⊢ Q + P := by
+  intro _ ⟨A, B, _, _, _⟩; exists B, A; and_intros; { trivial }; { trivial };
+  apply rmadd_comm'; trivial
+
+/-- `+` is commutative -/
+instance RProp.instAddCommMagma : AddCommMagma (RProp ρ) where
+  add_comm := by intro _ _; ext; constructor <;> apply add_comm'
 
 /-- `+` is associative -/
 private lemma add_assoc' : (P + Q) + R ⊢ P + (Q + R) := by
